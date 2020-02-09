b85b1cf531b0fa8406e679a5262da4f59ee5f488
LukasBrass <lukas.brasseleur@gmail.com>
2020-02-09 21:33:01 +0100

    Remove leagueName as it is not used anymore

--- BeFoot/Scenes/Home/MatchDetailsViewController.swift
+++ BeFoot/Scenes/Home/MatchDetailsViewController.swift
@@ -9,16 +9,13 @@
 import UIKit
 
 class MatchDetailsViewController: UIViewController {
-    
-    @IBOutlet var leagueName: UILabel!
-    
+        
     @IBOutlet var teamName1: UILabel!
     @IBOutlet var teamName2: UILabel!
     @IBOutlet var teamScore1: UILabel!
     @IBOutlet var teamScore2: UILabel!
     @IBOutlet var energy1: UILabel!
     @IBOutlet var energy2: UILabel!
-    @IBOutlet var coteList: UITableView!
     @IBOutlet var homeOdd: UILabel!
     @IBOutlet var drawOdd: UILabel!
     @IBOutlet var awayOdd: UILabel!
