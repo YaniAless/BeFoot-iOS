2cf98fe475891e474ffb56aa945716c0639be9cb
Alessandro Alterno <alternonolimit@gmail.com>
2020-02-09 17:43:45 +0100

    Added testing

--- BeFootTests/BeFootTests.swift
+++ BeFootTests/BeFootTests.swift
@@ -10,6 +10,14 @@ import XCTest
 @testable import BeFoot
 
 class BeFootTests: XCTestCase {
+    
+    var leagueService: LeagueService {
+        return LeagueServiceApi()
+    }
+    
+    var matchService: MatchService {
+        return MatchServiceApi()
+    }
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,15 +26,44 @@ class BeFootTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testLeagueService(){
+        
+        var rankSize: Int = 0
+        let expectation = self.expectation(description: "league")
+        
+        self.leagueService.getLeagueStanding(leagueId: 525) { rank in
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+            XCTAssertEqual(rankSize, 0)
+            rankSize = rank.teams.count
+            XCTAssertEqual(rank.teams.count, 20)
+            
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+        
+    }
+    func testMatchService(){
+        
+        var matchCount: Int = 0
+        let expectation = self.expectation(description: "matches")
+        
+        self.matchService.getByDate(date: "2020-02-08", leagueId: 525){ matches in
+
+            XCTAssertEqual(matchCount, 0)
+            matchCount = matches.fixtures.count
+            XCTAssertEqual(matches.fixtures.count, 6)
+            
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+        
     }
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
+            testMatchService()
             // Put the code you want to measure the time of here.
         }
     }
